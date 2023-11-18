1f9222960df233df70460534daf646d4b3a7fdf6
mamun-opu <abmamun504@gmail.com>
2023-11-18 11:48:31 -0500

    main, management class from moodle

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+
+        //Test part 1
+        System.out.println("Test 1: ");
+        var management = new Management();
+        management.hireTest1();
+
+        // Test part 2
+        System.out.println("Test 2: ");
+        System.out.println("Employees description: ");
+        management.hireTest2();
+
+        // Test part 3
+        System.out.println("Test 3: ");
+        System.out.println("Employees description: ");
+        management.hireTest3();
     }
 }
