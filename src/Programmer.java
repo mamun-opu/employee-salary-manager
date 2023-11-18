bedcbc6298069923e661164c54463deb80623b82
mamun-opu <abmamun504@gmail.com>
2023-11-18 10:39:46 -0500

    programmer class getter-setter and construction

--- src/Programmer.java
+++ src/Programmer.java
@@ -1,2 +1,15 @@
-public class Programmer {
+public class Programmer extends Employee{
+    private int nbProjects;
+    public Programmer(String name, int birthYear, double monthlySalary, int rate, int nbProjects) {
+        super(name, birthYear, monthlySalary, rate);
+        this.nbProjects = nbProjects;
+    }
+
+    public void setNbProjects(int nbProjects) {
+        this.nbProjects = nbProjects;
+    }
+
+    public int getNbProjects() {
+        return nbProjects;
+    }
 }
