20118020a1f725cfb806b906e92aca0036efa362
mamun-opu <abmamun504@gmail.com>
2023-11-18 12:02:18 -0500

    Motorcycle

---
+++ src/Motorcycle.java
@@ -0,0 +1,15 @@
+public class Motorcycle extends Vehicle {
+    private boolean sidecar;
+    public Motorcycle(String make, String plate, String color, String category, boolean sidecar) {
+        super(make, plate, color, category);
+        this.sidecar = sidecar;
+    }
+
+    public void setSidecar(boolean sidecar) {
+        this.sidecar = sidecar;
+    }
+
+    public boolean isSidecar() {
+        return sidecar;
+    }
+}
