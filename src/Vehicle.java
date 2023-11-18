bf552ec2d98d6cd874c687ad0c0623ca3da91a69
mamun-opu <abmamun504@gmail.com>
2023-11-18 11:55:26 -0500

    Vehicle

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,3 +1,33 @@
 public class Vehicle {
-    private
+    private final String make;
+    private final String plate;
+    private String color;
+    private final String category;
+    public Vehicle(String make, String plate, String color, String category){
+        this.make = make;
+        this.plate = plate;
+        this.color = color;
+        this.category = category;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
 }
