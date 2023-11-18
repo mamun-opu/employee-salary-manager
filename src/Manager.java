b5be060e794c305e1013e995ed28a0886e9a0426
mamun-opu <abmamun504@gmail.com>
2023-11-18 10:19:59 -0500

    manager class getter and setter

--- src/Manager.java
+++ src/Manager.java
@@ -5,5 +5,22 @@ public class Manager extends Employee{
         super(name, birthYear, monthlySalary, rate);
         this.nbTravelDays = nbTravelDays;
         this.nbClients = nbClients;
+        System.out.println("We have a new employee");
+    }
+
+    public void setNbTravelDays(int nbTravelDays) {
+        this.nbTravelDays = nbTravelDays;
+    }
+
+    public void setNbClients(int nbClients) {
+        this.nbClients = nbClients;
+    }
+
+    public int getNbTravelDays() {
+        return nbTravelDays;
+    }
+
+    public int getNbClients() {
+        return nbClients;
     }
 }
